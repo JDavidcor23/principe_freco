ec404414a36ad6c111b97be865320ea479f97a3a
JDavidcor23 <jorgedavid23diaz@gmail.com>
2021-11-27 14:30:43 -0500

    modificar

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -19,3 +19,58 @@ const getUser = async () =>{
     })
 }
 window.addEventListener('DOMContentLoaded', getUser)
+
+const capturarProductos = () =>{
+    const nombre = document.getElementById('inputNombre').value;
+    const img1 = document.getElementById('img1').value;
+    const img2 = document.getElementById('img2').value;
+    const img3 = document.getElementById('img3').value;
+    const precio = document.getElementById('precio').value;
+    const descripcion = document.getElementById('inputDescripcion').value;
+
+      const producto = {
+            nombre,
+            img:[img1, img2, img3],
+            precio,
+            descripcion
+        }
+
+    return producto;
+}
+const btnProducto = document.getElementById("btnProducto");
+
+
+btnProducto.addEventListener('click', async() =>{
+    const nombre = document.getElementById('inputNombre').value;
+    const resp = await fetch(url);
+    const lista = await resp.json();
+    const buscado = lista.find(pro=> pro.nombre.toLocaleLowerCase() === nombre.toLocaleLowerCase())
+    if(buscado !== undefined){
+        const {id,img, descripcion, precio} = buscado
+        document.getElementById('img1').value = img[0]
+        document.getElementById('img2').value = img[1]
+        document.getElementById('img3').value =  img[2]
+        document.getElementById('precio').value = precio
+        document.getElementById('inputDescripcion').value =descripcion
+        document.getElementById('inputId').value =id
+        
+    }
+    else{
+        alert("correo no encontrado")
+    }
+})
+const btnModificar = document.getElementById("btnModificar");
+
+btnModificar.addEventListener("click", async () =>{
+    const capturaInfo = capturarProductos()
+
+    const id = document.getElementById("inputId").value;
+
+    await fetch(url + id, {
+        method: 'PUT',
+        body: JSON.stringify(capturaInfo),
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    })
+})
