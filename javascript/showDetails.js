8182f60e5f6b78a25326c49f6f4b6117b742150c
JDavidcor23 <jorgedavid23diaz@gmail.com>
2021-11-27 18:14:47 -0500

    cambios

--- javascript/showDetails.js
+++ javascript/showDetails.js
@@ -1,46 +1,54 @@
-export const showDetails = async(element,productEndpoint) => {
-    const products = await productEndpoint;
+const detalles = document.querySelector(".detalles")
+let carrito = []
+const showDetails =(element) => {
+    const products = [JSON.parse(localStorage.getItem("Detalle"))]
     products.forEach(prod => {
-        const {id,img,nombre, precio, descripcion} = prod;
-        element.innerHTML += `
-        <div id="slider-mobile"></div>
-        <div id="list-images">
-            <img src= ${img[0]} alt="produc image">
-            <img src= ${img[1]} alt="produc image">
-            <img src= ${img[2]} alt="produc image"> 
-        </div>
-        <div id="carrusel-images">
-            <div class="product-image" id="uno"></div>
-            <div class="product-image" id="dos"></div>
-            <div class="product-image" id="tres"></div>          
-        </div>
-        <div id="detalle-container">
-            <div id="name-container">
-                <h2>${nombre}</h2>
-            </div>            
-            <div id="price"><span>$</span>${precio}</div>
-            <div id="size">
-                <p>Size</p>
-                <div id="size-content">
-                    <ul id="size-list">
-                        <li class="size-active">S</li>
-                        <li>M</li>
-                        <li>L</li>
-                        <li>XL</li>
-                        <li>XXL</li>
-                    </ul>
-                </div>
-            </div>
-            <div id="butons">
-                <button id="addCard">ADD TO CART</button>
-                <button id="buy">BUY IT NOW</button>
-            </div>
-            <div id="description">
-                <p>${descripcion}</p>
-                <a href="#">Click for Sizing</a>
-            </div>
-        </div>
-        `
+         const {id,img,nombre, precio, descripcion} = prod;
+         element.innerHTML = `
+         <div id="carrusel-images">
+             <img src="${img[0]}" alt="">
+             <img src="${img[1]}" alt="">
+             <img src="${img[2]}" alt="">         
+         </div>
+         <div id="detalle-container">
+             <div id="name-container">
+                 <h2>${nombre}</h2>
+             </div>            
+             <div id="price"><span>$</span>${precio}</div>
+             <div id="size">
+                 <p>Size</p>
+                 <div id="size-content">
+                     <ul id="size-list">
+                         <li class="size-active">S</li>
+                         <li>M</li>
+                         <li>L</li>
+                         <li>XL</li>
+                         <li>XXL</li>
+                     </ul>
+                 </div>
+             </div>
+             <div id="butons">
+                 <button id="addCard">ADD TO CART</button>
+                 <button id="buy">BUY IT NOW</button>
+             </div>
+             <div id="description">
+                 <p>${descripcion}</p>
+                 <a href="#">Click for Sizing</a>
+             </div>
+         </div>
+         `
+         let numeroItems = 0
+         const btnCarrito = document.getElementById("addCard")
+         btnCarrito.addEventListener('click', async(e) =>{
+             numeroItems+=1
+             let items =0 
+             items += products
+             let productoCarrito={
+                 numeroItems,
+                 items,
+             }
+             localStorage.setItem("ItemsCarrito", JSON.stringify(productoCarrito))
+         })
     })
 }
-  
+window.addEventListener('DOMContentLoaded',showDetails(detalles))
